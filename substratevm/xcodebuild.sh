5292cfbd713ccbf76cec8a6fbb878a76abb8a3ce
Andrey Mikhalev <andrey.mikhalev@delightex.com>
2021-07-28 12:08:51 +0300

    Rename schemes

--- substratevm/xcodebuild.sh
+++ substratevm/xcodebuild.sh
@@ -1,15 +1,14 @@
 # iOS
-xcodebuild -sdk iphoneos -arch arm64 -project graal-svm-ios.xcodeproj -scheme graal-svm-arm64-ios-debug
-xcodebuild -sdk iphoneos -arch arm64 -project graal-svm-ios.xcodeproj -scheme graal-svm-arm64-ios-release
+xcodebuild -sdk iphoneos -arch arm64 -project graal-svm-ios.xcodeproj -scheme graal-svm-arm64-debug
+xcodebuild -sdk iphoneos -arch arm64 -project graal-svm-ios.xcodeproj -scheme graal-svm-arm64-release
 
 # iOS Simulator
-xcodebuild -sdk iphonesimulator -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-ios-sim-debug
-xcodebuild -sdk iphonesimulator -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-ios-sim-release
+xcodebuild -sdk iphonesimulator -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-debug
+xcodebuild -sdk iphonesimulator -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-release
 
 # Mac Catalyst
-xcodebuild -sdk macosx -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-ios-sim-debug
-xcodebuild -sdk macosx -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-ios-sim-release
-
+xcodebuild -sdk macosx -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-debug
+xcodebuild -sdk macosx -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-release
 #xcodebuild -sdk macosx -arch arm64 -project graal-svm-ios.xcodeproj -scheme graal-svm-arm64-ios-debug
 #xcodebuild -sdk macosx -arch arm64 -project graal-svm-ios.xcodeproj -scheme graal-svm-arm64-ios-release
 
