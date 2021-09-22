644e55f6fd5bc45b56818b94a3796ff9e08735a5
Kirill Prazdnikov <Kirill.Prazdnikov@delightex.com>
2021-09-22 18:54:23 +0300

    comment Mac Catalyst

--- substratevm/xcodebuild.sh
+++ substratevm/xcodebuild.sh
@@ -6,9 +6,9 @@ xcodebuild -sdk iphoneos -arch arm64 -project graal-svm-ios.xcodeproj -scheme gr
 xcodebuild -sdk iphonesimulator -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-debug
 xcodebuild -sdk iphonesimulator -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-release
 
-# Mac Catalyst
-xcodebuild -sdk macosx -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-debug
-xcodebuild -sdk macosx -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-release
+# Mac Catalyst (experimental)
+#xcodebuild -sdk macosx -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-debug
+#xcodebuild -sdk macosx -arch x86_64 -project graal-svm-ios.xcodeproj -scheme graal-svm-x86-64-release
 #xcodebuild -sdk macosx -arch arm64 -project graal-svm-ios.xcodeproj -scheme graal-svm-arm64-ios-debug
 #xcodebuild -sdk macosx -arch arm64 -project graal-svm-ios.xcodeproj -scheme graal-svm-arm64-ios-release
 
@@ -18,7 +18,7 @@ lipo -info xcode/graal-svm-arm64-ios-d.a
 lipo -info xcode/graal-svm-arm64-ios-r.a
 lipo -info xcode/graal-svm-x86-64-ios-simulator-d.a
 lipo -info xcode/graal-svm-x86-64-ios-simulator-r.a
-lipo -info xcode/graal-svm-x86-64-mac-catalyst-d.a
-lipo -info xcode/graal-svm-x86-64-mac-catalyst-r.a
+#lipo -info xcode/graal-svm-x86-64-mac-catalyst-d.a
+#lipo -info xcode/graal-svm-x86-64-mac-catalyst-r.a
 #lipo -info xcode/graal-svm-arm64-mac-catalyst-d.a
 #lipo -info xcode/graal-svm-arm64-mac-catalyst-r.a
